6ecd452f43a7be4228976dde9cd15cdfebfa69ac
yherson <yherson_fsf@outlook.com>
2015-12-14 08:22:00 -0500

    start skills
    empezando con skills

--- public/js/border.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-   var $browser = $.browser.name;
-   var $version = $.browser.versionNumber;
-   if ($browser == "android" && $version <= 4) {
-      $("body").addClass("no-support-border");
-   }
-   // alert("mi navegador es: " + $browser + " y la version es: " + $version);
-})
